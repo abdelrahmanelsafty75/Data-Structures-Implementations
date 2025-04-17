67d05f4e22b1c83f8983c38063e74302bf9cde21
Abdelrahman Elsafty <elsafty@gmail.com>
2025-04-17 22:59:01 +0200

    adding comments for clarity

--- queues/queue_dynamic_array.cpp
+++ queues/queue_dynamic_array.cpp
@@ -1,67 +1,3 @@
-//#include <iostream>
-//using namespace std;
-//#define size 5
-//
-//int queue[size];
-//int front = -1, rear = -1;
-//
-//bool isEmpty() {
-//    return((front == -1 and rear == -1) || front > rear);
-//}
-//bool isFull() {
-//    return(rear == size - 1);
-//}
-//void enqueue(int val) {
-//    if (isFull())
-//        cout << "Queue is already full.\n";
-//    else {
-//        if (isEmpty()) {
-//            front++;
-//            queue[++rear] = val;
-//        }
-//        else {
-//            queue[++rear] = val;
-//        }
-//    }
-//}
-//void dequeue() {
-//    if (isEmpty())
-//        cout << "Queue is already empty.\n";
-//    else {
-//        front++;
-//    }
-//}
-//int peak() {
-//    if (isEmpty()) {
-//        cout << "Queue is already empty.\n";
-//        return -1;
-//    }
-//    else {
-//        return queue[front];
-//    }
-//}
-//void display() {
-//    if (isEmpty()) 
-//        cout << "Queue is already empty.\n";
-//    else {
-//        for (int i = front; i <= rear; i++) {
-//            cout << queue[i] << " ";
-//        }
-//        cout << endl;
-//    }
-//}
-//int main(){
-//    cout << "Hello with queue ^_^ : \n\n";
-//
-//    enqueue(5);
-//    enqueue(10);
-//    enqueue(25);
-//    display();
-//    dequeue();
-//    cout << "peak: " << peak();
-//    return 0;
-//}
-/*================================ Dynamic Array =======================================================*/
 #include <iostream>
 using namespace std;
 
@@ -70,20 +6,20 @@ public:
 	int front, rear, len;
 	int* queue;
 
-    Queue(int size) {
+    Queue(int size) { // Constructor to set initial values
 		queue = new int[size];
 		front = -1;
 		rear = -1;
 		len = size;
 	}
-	bool isempty() {
+	bool isempty() {  // check if queue is empty
 		return (front == -1 or front > rear);
 	}
-	bool isfull() {
+	bool isfull() {  // check if queue is full
 		return (rear == len - 1);
 	}
 
-	void enqueue(int val) {
+	void enqueue(int val) { // adding item
 		if (isfull())
 			cout << "Queue is full\n";
 		else if (isempty()) {
@@ -93,16 +29,16 @@ public:
 		else
 			queue[++rear] = val;
 	}
-	void dequeue() {
+	void dequeue() {  // deleting item
 		if (isempty())
 			cout << "Queue is already empty\n";
 		else
 			front++;
 	}
-	void peak() {
+	void peak() {  // return the first item
 		cout << "peak: " << queue[front] << endl;
 	}
-	void display() {
+	void display() {  // display all items
 		if (isempty())
 			cout << "Queue is already empty\n";
 		else {
@@ -115,7 +51,7 @@ public:
 };
 
 int main() {
-	cout << "Hello Nigga let's practice: ^_^ \n\n";
+	cout << "Hello, let's practice: ^_^ \n\n";
 
 
 	Queue q(3);
